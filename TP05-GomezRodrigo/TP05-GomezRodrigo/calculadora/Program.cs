aa74915a4164f8dbcc05b62cbb82b9f4fd9cc95f
Roderick <iroderick@outlook.es>
2022-06-04 13:06:13 -0300

    calculadorav2 completa

--- TP05-GomezRodrigo/TP05-GomezRodrigo/calculadora/Program.cs
+++ TP05-GomezRodrigo/TP05-GomezRodrigo/calculadora/Program.cs
@@ -1,11 +1,11 @@
 ﻿int operacion, flag;
-double operando1, operando2;
+double operando1, operando2, resultado = 0; //por que me obliga a inicializar solo esta variable
+
 const double INDEFINIDO = -999999;
 
 
-void CalculadoraV1(int operacion, double op1, double op2)
+double CalculadoraV1(int operacion, double op1, double op2)
 {
-    double resultado = 0; //por que me obliga a inicializar solo esta variable
     switch (operacion)
     {
         case 0:
@@ -31,14 +31,31 @@ void CalculadoraV1(int operacion, double op1, double op2)
             }
             break;
     }
-    if (resultado == INDEFINIDO)
-    {
-        Console.WriteLine("No está definida la división por 0!");
-    }
-    else
-    {
-        Console.WriteLine($"El resultado de la operacion es {resultado}");
-    }
+    return resultado;
+}
+
+void CalculadoraV2()
+{
+    double numero, num1, num2;
+
+    Console.Clear();
+    Console.WriteLine("Ingrese un número para operar: ");
+    numero = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"El número ingresado es: {numero}");
+    Console.WriteLine($"Valor absoluto: {Math.Abs(numero)}");
+    Console.WriteLine($"Cuadrado: {Math.Pow(numero, 2)}");
+    Console.WriteLine($"Raíz cuadrada: {Math.Sqrt(numero)}");
+    Console.WriteLine($"Seno: {Math.Sin(numero)}");
+    Console.WriteLine($"Coseno: {Math.Cos(numero)}");
+    Console.WriteLine($"Parte entera: {Math.Truncate(numero)}");
+
+    Console.WriteLine("Ingrese un número: ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Ingrese otro número: ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"El número mas grande entre los últimos dos ingresados es: {Math.Max(num1, num2)}");
+    Console.WriteLine($"El número mas chico entre los últimos dos ingresados es: {Math.Min(num1, num2)}");
 }
 
 do
@@ -53,6 +70,15 @@ do
     Console.Clear();
 
     CalculadoraV1(operacion, operando1, operando2);
+    if (resultado == INDEFINIDO)
+    {
+        Console.WriteLine("No está definida la división por 0!");
+    }
+    else
+    {
+        Console.WriteLine($"El resultado de la operacion es {resultado}");
+    }
+    CalculadoraV2();
 
     Console.WriteLine("Desea seguir usando la calculadora? [0]:NO - [1]:SI");
     flag = Convert.ToInt32(Console.ReadLine());
