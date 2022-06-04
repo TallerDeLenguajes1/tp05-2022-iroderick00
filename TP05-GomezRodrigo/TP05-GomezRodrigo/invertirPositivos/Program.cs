5f954a6cbb1be933516d0f52e9557bdaf3cad465
Roderick <iroderick@outlook.es>
2022-06-04 11:05:22 -0300

    ejercicio invertir positivos

---
+++ TP05-GomezRodrigo/TP05-GomezRodrigo/invertirPositivos/Program.cs
@@ -0,0 +1,25 @@
+﻿int numero, cociente, resto;
+string numeroParaMostrar = "";
+
+Console.WriteLine("Ingrese el numero a invertir: ");
+numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"El número ingresado es: {numero}");
+
+cociente = numero;
+if (numero <= 0)
+{
+    Console.Clear();
+    Console.WriteLine("Solo está permitido invertir números positivos");
+}
+else
+{
+    while (cociente > 0)
+    {
+        resto = cociente % 10;
+        cociente = cociente / 10;
+        numeroParaMostrar += resto.ToString();
+    }
+    Console.Clear();
+    Console.WriteLine($"El número ingresado es: {numero}");
+    Console.WriteLine($"El número invertido es: {numeroParaMostrar}");
+}
